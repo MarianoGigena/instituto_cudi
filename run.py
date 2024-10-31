ff11f610dc393ca1801e8e922c45d765a249beed
MarianoGigena <marianogigena@gmail.com>
2024-10-31 10:40:37 -0300

    7to deploy en netlify

--- run.py
+++ run.py
@@ -4,4 +4,7 @@ app = create_app()
 
 if __name__ == "__main__":
     # app.run(debug=True)
-    app.run(host="0.0.0.0", port=5000)
+    # app.run(host="0.0.0.0", port=5000)
+    from waitress import serve
+
+    serve(app, host="0.0.0.0", port=8080)
