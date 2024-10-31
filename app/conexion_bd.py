11d5d78f61e7612af7765d1e5f2bd5d65cbb915d
MarianoGigena <marianogigena@gmail.com>
2024-10-31 09:57:18 -0300

    6to bd en clever cloude

--- app/conexion_bd.py
+++ app/conexion_bd.py
@@ -3,8 +3,8 @@ import mysql.connector
 
 def obtener_conexion():
     return mysql.connector.connect(
-        host="127.0.0.1",
-        user="root",
-        password="3793",
-        database="instituto_cudi2",
+        host="bpwepioek9qlpkrlo58y-mysql.services.clever-cloud.com",
+        user="ufrojjoexr0lrd1y",
+        password="UrDrhOOhFRzlF3r3FVYI",
+        database="bpwepioek9qlpkrlo58y",
     )
