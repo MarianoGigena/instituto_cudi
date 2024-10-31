54745cf86f744742337f22524a5ea8c76990aeeb
MarianoGigena <marianogigena@gmail.com>
2024-10-31 10:49:23 -0300

    7to deploy

--- run.py
+++ run.py
@@ -4,7 +4,7 @@ app = create_app()
 
 if __name__ == "__main__":
     # app.run(debug=True)
-    # app.run(host="0.0.0.0", port=5000)
-    from waitress import serve
+    app.run(host="0.0.0.0", port=5000)
+    # from waitress import serve
 
-    serve(app, host="0.0.0.0", port=8080)
+    # serve(app, host="0.0.0.0", port=8080)
